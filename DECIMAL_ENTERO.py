e8d11153393152abc1549cb5c87fa3d0dd620eb8
yair117 <80426321+yair117@users.noreply.github.com>
2023-11-21 22:20:34 -0600

    Add files via upload

---
+++ DECIMAL_ENTERO.py
@@ -0,0 +1,10 @@
+"""
+Diseñe un algoritmo que escriba un numero. si el numero es entero que escriba
+(EL NUMERO ES ENTERO) En caso contrario (El numero no es entero)
+"""
+from math import*
+numero=float(input("INGRESA UN NUMERO:\n"))
+if numero==int(numero):
+    print("EL NUMERO ES ENTERO PERRO")
+else:
+    print("EL NUMERO NO ES ENTERO")
